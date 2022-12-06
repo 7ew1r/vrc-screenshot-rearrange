0d6528ebff4353e2ff0be267568a76098e06fa3d
7ew1r <art.sht.0317@gmail.com>
2022-12-06 16:30:47 +0900

    udpate

---
+++ README.md
@@ -0,0 +1,21 @@
+# vrc-screenshot-rearrange
+
+VRChat Screenshot Renaming Tool
+
+Renaming old VRChat screenshot names
+
+```
+2019-03/VRChat_1920x1080_2019-03-10_19-39-22.076.png
+```
+
+to
+
+```
+2019-03/VRChat_2019-03-10_19-39-22.076_1920x1080.png
+```
+
+# Usage
+
+```bash
+python3 main.py [VRChat_screenshot_dir_path]
+```
--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import glob
 import re
 import os
-import shutil
+import sys
 
-# VRChat のスクリーンショットのフォルダを指定
-user_dir = os.path.expanduser('~')
-vrc_screenshot_path = os.path.join(
-    user_dir, "Library/Mobile Documents/com~apple~CloudDocs/VRChat")
+if(len(sys.argv) < 2):
+     print("not enough args")
+     sys.exit(1)
+
+vrc_screenshot_path = os.path.abspath(sys.argv[1])
+
+if not (os.path.isdir(sys.argv[1])):
+    print(f'{sys.argv[1]} is not directory')
+    sys.exit(1)
 
 
-def check_screenshot_name(file_name):
-    pattern = r'VRChat_\d{4}x\d{3,4}_\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2}.\d{3}.png'
+def check_screenshot_name(file_name, pattern):
+    # pattern = r'VRChat_\d{4}x\d{3,4}_\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2}.\d{3}.png'
     result = re.match(pattern, file_name)
 
     if result:
@@ -19,22 +24,38 @@ def check_screenshot_name(file_name):
         return False
 
 
-def get_year_month(file_name):
-    pattern = r'VRChat_\d{4}x\d{3,4}_(\d{4}-\d{2})-\d{2}_\d{2}-\d{2}-\d{2}.\d{3}.png'
-    result = re.match(pattern, file_name)
-    return result.group(1)
+def get_file_data(file_name):
+     pattern = r'VRChat_(\d{4}x\d{3,4})_(\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2}.\d{3}).(png)'
+     result = re.match(pattern, file_name)
+     return result.group(1), result.group(2), result.group(3)
 
 
-files = glob.glob(vrc_screenshot_path + "/*")
+# VRChat_2022-12-06_01-53-30.150_3840x2160.png
+def create_new_file_name(resolution, datetime, extension):
+    return f'VRChat_{datetime}_{resolution}.{extension}'
 
 
-for file in files:
-    file_name = os.path.basename(file)
-    if not check_screenshot_name(file_name):
-        continue
+items = os.listdir(vrc_screenshot_path)
+year_month_dirs =[f for f in items if os.path.isdir(os.path.join(vrc_screenshot_path, f))]
 
-    year_month = get_year_month(file_name)
+for dir in year_month_dirs:
+    dir_path = os.path.join(vrc_screenshot_path, dir)
+    files = glob.glob(dir_path + "/*.png")
 
-    year_month_dirname = os.path.join(vrc_screenshot_path, year_month)
-    os.makedirs(year_month_dirname, exist_ok=True)
-    shutil.move(file, year_month_dirname)
+    for file in files:
+        file_name = os.path.basename(file)
+
+        # VRChat_1920x1080_2019-12-01_03-18-48.414.png
+        old_file_name_pattern = r'VRChat_\d{4}x\d{3,4}_\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2}.\d{3}.png'
+        if not check_screenshot_name(file_name, old_file_name_pattern):
+            print(f'not change: {file_name}')
+            continue
+
+        resolution, datetime, extension = get_file_data(file_name)
+        new_file_name = create_new_file_name(resolution, datetime, extension)
+
+        old_file_path = os.path.join(dir_path, file_name)
+        new_file_path = os.path.join(dir_path, new_file_name)
+        print(f'rename: {file_name} => {new_file_name}')
+        os.rename(old_file_path, new_file_path)
+
